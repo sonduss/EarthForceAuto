d7ac0cc2502b89bb7548bc97b50c6761a665e80c
sonduss <sondusabughdaib@gmail.com>
2024-03-17 12:30:36 +0200

    First code for login page

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -6,5 +6,12 @@ describe('Login Test', () => {
       cy.get('.inline-flex').click();
       cy.get('.text-white.text-xl').should('be.visible');
     });
+    it('should display an error message with invalid credentials', () => {
+      cy.visit('https://portal.dev.earthforce.io/portal/overview');
+      cy.get('#\\:r0\\:\\-form\\-item').type("muathmoh@gmail.com");
+      cy.get('#\\:r1\\:-form-item').type("Erth@1234");
+      cy.get('.inline-flex').click();
+      cy.get('.ml-4').should('be.visible') 
+    })
   
 });
