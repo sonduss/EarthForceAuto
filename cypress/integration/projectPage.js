0bc37f6d92a28a84648d27a693ce0945ba319428
SondusAbuGhdaib <83163825+SondusAbuGhdaib@users.noreply.github.com>
2024-04-15 14:28:00 +0300

    Intercept Changes

--- cypress/integration/projectPage.js
+++ cypress/integration/projectPage.js
@@ -2,74 +2,53 @@ import Overview from '../support/PageObjectModal/Overview.js';
 import ProjectPage from '../support/PageObjectModal/ProjectPage.js';
 
 describe('Project Page Tests', () => {
-const overview = new Overview();
-const project=new ProjectPage();
-beforeEach(() => {
-cy.viewport(1024, 768);
-cy.login();
-});
+  const overview = new Overview();
+  const project = new ProjectPage();
 
-it('Project Metrics Page', () => {
-cy.wait(5000);
-overview.getMapMArker().should('exist').then(marker => {
-cy.wrap(marker).find('p').eq(0).should('exist').then(paragraph => {
-const markerText = paragraph.text();
-overview.getMapMArker().eq(0).click();
-project.getProjectHeader().invoke('text').then(text => {
-expect(text).to.equal(markerText);
-});
-project.getProjectName().invoke('text').then(projectName => {
-expect(projectName).to.equal(markerText);
-});
-project.progressCard().should('be.visible');
-project.totalAcres().should('be.visible');
-project.avgAcres().should('be.visible');
-project.projectHours().should('be.visible');
-project.activeUnits().should('be.visible');
-});
-});
-cy.getProjectID().then(projectID => {
-cy.LoginAPITest().then(token => {
-const query = `
-query GetProjectMetrics($projectId: String!) {
-getProjectMetrics(projectId: $projectId) {
-totalAcres {
-totalAcresDiff
-totalAcres
-totalAcresLast7Days
-}
-acresPerDay {
-avgAcresPerDay
-avgAcresDiff
-}
-projectHours {
-totalProjectHours
-projectHoursLast7Days
-}
-activeUnits {
-totalActiveUnits
-activeUnitsDiff
-}
-}
-}
-`;
-const variables = {
-projectId: projectID
-};
-cy.request({
-method: 'POST',
-url: 'https://api.dev.earthforce.io/portal/main-api',
-body: {
-query,
-variables
-},
-headers: {
-Authorization: token
-}
-}).then((response) => {
-expect(response.status).to.eq(200);
-const projectMetrics=response.body.data.getProjectMetrics
-const totalAcres = projectMetrics.totalAcres.totalAcres;
+  beforeEach(() => {
+    cy.viewport(1024, 768);
+    cy.login();
+  });
+
+  it('Project Metrics Page', () => {
+    cy.wait(5000);
+    cy.intercept('POST', 'https://api.dev.earthforce.io/portal/main-api', (req) => {
+      console.log("This is for testing1")
+      console.log("Intercepted request body:", req.body);
+      console.log("The operation name is", req.body.operationName)
+      if (req.body && req.body.operationName === "GetProjectMetrics") {
+        req.alias = 'getProjectMetricsQuery'; 
+        console.log("This is for testing3333")
+      }
+    })
+
+    overview.getMapMArker().should('exist').then(marker => {
+      cy.wrap(marker).find('p').eq(0).should('exist').then(paragraph => {
+        const markerText = paragraph.text();
+        overview.getMapMArker().eq(0).click();
+        project.getProjectHeader().invoke('text').then(text => {
+          expect(text).to.equal(markerText);
+        });
+        project.getProjectName().invoke('text').then(projectName => {
+          expect(projectName).to.equal(markerText);
+        });
+        project.progressCard().should('be.visible');
+        project.totalAcres().should('be.visible');
+        project.avgAcres().should('be.visible');
+        project.projectHours().should('be.visible');
+        project.activeUnits().should('be.visible');
+      });
+    });
+
+    cy.wait('@getProjectMetricsQuery').then((interception) => {
+        console.log("This is for testing222")
+        console.log("Intercepted request:", interception.request)
+        console.log("Intercepted response:", interception.response)
+        console.log("This is response",interception.response.statusCode)
+        const response = interception.response;
+        const projectMetrics = response.body.data.getProjectMetrics;        
+      expect(interception.response.statusCode).to.eq(200);
+      const totalAcres = projectMetrics.totalAcres.totalAcres;
 const totalAcressDiff=projectMetrics.totalAcres.totalAcresDiff;
 const acresPerDay = projectMetrics.acresPerDay.avgAcresPerDay;
 const AvgAcresDiff = projectMetrics.acresPerDay.avgAcresDiff;
@@ -81,7 +60,10 @@ const activeUnitsDiff = projectMetrics.activeUnits.activeUnitsDiff;
 project.getActualtotalAcres().invoke('text').then(actualTotalAcres => {
 const roundedTotalAcres = parseFloat(totalAcres.toFixed(2));
 const textWithoutAC = actualTotalAcres.replace("Ac", "");
-expect(roundedTotalAcres).to.eq(parseFloat(textWithoutAC));})
+console.log("Actual",textWithoutAC)
+console.log("expected",roundedTotalAcres)
+expect(roundedTotalAcres).to.eq(parseFloat(textWithoutAC));
+})
 
 project.getActualAcresDiff().invoke('text').then(actualAcresDiff => {
 const totalAcressDiffRound=Math.floor(Math.abs(totalAcressDiff))
@@ -109,8 +91,6 @@ expect(totalActiveUnits).to.eq(parseFloat(actualActiveUnits));
 });
 project.getActualActiveUnitsDiff().invoke('text').then(actualActiveUnitsDiff => {
 expect(activeUnitsDiff).to.eq(parseFloat(actualActiveUnitsDiff));
-});
-});
 });
 });
 });
